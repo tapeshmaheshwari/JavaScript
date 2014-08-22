e597eba8861c46d7d41ba04a325a2bfad68e1fd6
tapesh maheshwari <tapesh.maheshwari@gmail.com>
2014-08-23 01:02:46 +0530

    removing old file

--- test.js
+++
@@ -1,7 +0,0 @@
-function assert(condition, text)
-{
-	var entry = document.createElement("li");
-	entry.innerHTML = text;
-	entry.classList.add(condition ? "pass" : "fail");
-	document.getElementById("result").appendChild(entry);
-}
